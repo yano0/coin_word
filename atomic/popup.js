f7027032a592025d53ead0613dfef271f3fc8208
MonstersoulDev1 <kiruazorudexick@gmail.com>
2024-02-07 21:11:07 +0900

    造語の結果反映

--- atomic/popup.js
+++ atomic/popup.js
@@ -25,7 +25,7 @@ generate.addEventListener('click', () => {
     })
     .then(data => {
         console.log(data); // サーバーからの応答をコンソールに表示します
-        // ここで必要な処理を行います
+        document.getElementById("output").value = data["zougo"];
     })
     .catch(error => {
         console.error("Fetchエラー:", error);
