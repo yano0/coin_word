625b4c698af1b43c7192bc8edd0ae6c07a107f0c
yano0 <yanonay3@gmail.com>
2024-02-07 09:00:02 +0000

    click shitai

--- yano/popup.js
+++ yano/popup.js
@@ -1,11 +1,17 @@
-const generate = document.getElementById('generate')
-generate.addEventListener('click', () => {
-    console.log('clicked')
-    chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-            fetch('https://zougogenebackend.onrender.com/welcome/') // ローカルのPythonサーバーにリクエストを送信
-                .then(response => response.json())
-                .then(data => sendResponse(data))
-                .catch(error => console.error(error));
-            return true;
-    });
-})
+const generate = document.getElementById("generate")
+if (generate) {
+    console.log('found')
+    generate.addEventListener('click', () => {
+        console.log('clicked')
+        chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+                fetch('https://zougogenebackend.onrender.com/welcome/') // ローカルのPythonサーバーにリクエストを送信
+                    .then(response => response.json())
+                    .then(data => sendResponse(data))
+                    .catch(error => console.error(error));
+                return true;
+        });
+    })
+}
+else {
+    console.log('not found')
+}
