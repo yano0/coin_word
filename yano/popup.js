5f8952ddf60e7aa04a7a5469a170d60658daf193
yano0 <yanonay3@gmail.com>
2024-02-07 08:49:55 +0000

    コードの変更を記述するための短く簡潔なコミットメッセージ

--- yano/popup.js
+++ yano/popup.js
@@ -1,1 +1,11 @@
-console.log("This is a popup!")
+const generate = document.getElementById('generate')
+generate.addEventListener('click', () => {
+    console.log('clicked')
+    chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+            fetch('https://zougogenebackend.onrender.com/welcome/') // ローカルのPythonサーバーにリクエストを送信
+                .then(response => response.json())
+                .then(data => sendResponse(data))
+                .catch(error => console.error(error));
+            return true;
+    });
+})
