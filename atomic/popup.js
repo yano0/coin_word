fd3a662c5131db58a0d82eb43dce9ea7214eb7f1
MonstersoulDev1 <kiruazorudexick@gmail.com>
2024-02-07 19:06:31 +0900

    fixed syntax

--- atomic/popup.js
+++ atomic/popup.js
@@ -1,6 +1,37 @@
-console.log("This is a popup!")
-
-function clickFunction(){
-    let output = document.getElementsByClassName("output")
-    output.innerText = 'ボタンが押されました';
-}
+const generate = document.getElementById("generate")
+generate.addEventListener('click', () => {
+    console.log('clicked');
+    var inputText = document.getElementById("inputText").value;
+    // 入力が空でないことを確認
+    if(inputText.trim() !== '') {
+        // APIに送信するデータを作成
+        var input = {
+        text: inputText
+        };
+    fetch("https://zougogenebackend.onrender.com/generate_zougo/",{
+        method: "POST",
+        body:JSON.stringify(input),
+        headers:{"Content-Type": "application/json"}}
+    ).then(response => {
+        if (!response.ok) {
+        throw new Error("サーバーエラー: " + response.status);
+        }
+        return response.json(); // 応答をJSON形式として解析
+    })
+    .then(data => {
+        console.log(data); // サーバーからの応答をコンソールに表示します
+        // ここで必要な処理を行います
+    })
+    .catch(error => {
+        console.error("Fetchエラー:", error);
+    });
+    // chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    //         fetch('https://zougogenebackend.onrender.com/welcome/') // ローカルのPythonサーバーにリクエストを送信
+    //             .then(response => response.json())
+    //             .then(data => sendResponse(data))
+    //             .catch(error => console.error(error));
+    //         return true;
+    // });
+    console.log(response);
+    }
+});
